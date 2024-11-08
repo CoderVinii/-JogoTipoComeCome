1879dbdcf248b8b23d40194cc4214522a6d0c3a5
CoderVinii <viniciusdmfernandes@gmail.com>
2024-11-08 20:08:39 -0300

    Atualizacoes

--- JogoFGFG/mapa.h
+++ JogoFGFG/mapa.h
@@ -4,11 +4,20 @@ struct mapa {
     int colunas;
 };
 
-typedef struct mapa MAPA;
+typedef struct mapa MAPA;\
+
+struct posicao {
+    int x;
+    int y;
+
+};
+
+typedef struct posicao POSICAO;
 
 void alocamapa(MAPA* m);
 void lemapa(MAPA* m);
 void liberamapa(MAPA* m);
 void imprimemapa(MAPA* m);
+void encontramapa(MAPA* m, POSICAO* p, char c);
 
 
